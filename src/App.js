3e7e7ff45fdd0e46736f2e615f61a9a80b762cfc
Keerthu987 <keerthananilasha111@gmail.com>
2025-02-12 20:28:38 +0530

    netflix-gpt-1stcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+<link rel="stylesheet" href="./index.css" />
+
+
+function App() {
+  return (
+    <div className="font-bold text-center text-5xl text-red-400">
+      Netflis
+    </div>
+  );
+}
+
+export default App;
